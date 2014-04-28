d74a083bcdbbb4dc532f3aa4e90825c7867a8444
Ivan Krutov <vania-pooh@vania-pooh.com>
2014-04-28 20:26:47 +0400

    Fixed AllureAdapter loading issue

--- example/src/StackTest.php
+++ example/src/StackTest.php
@@ -3,6 +3,7 @@
 namespace src;
 
 use PHPUnit_Framework_TestCase;
+use Yandex\Allure\Adapter\Annotation\Features;
 use Yandex\Allure\Adapter\Annotation\Step;
 use Yandex\Allure\Adapter\Annotation\Title;
 
@@ -14,18 +15,37 @@ use Yandex\Allure\Adapter\Annotation\Title;
 class StackTest extends PHPUnit_Framework_TestCase
 {
     /**
-     * @Title("Stack push and pop operations test")
-     * @Step("Core stack operations")
+     * @Title("Stack creation test")
+     * @Step("Core operations")
+     * @Features({"Initialization"})
      */
-    public function testPushAndPop()
+    public function testCreate()
     {
         $stack = array();
         $this->assertEquals(0, count($stack));
+    }
 
+    /**
+     * @Title("Stack push operation test")
+     * @Step("Core operations")
+     * @Features({"Write operation"})
+     */
+    public function testPush()
+    {
+        $stack = array();
         array_push($stack, 'foo');
         $this->assertEquals('foo', $stack[count($stack)-1]);
         $this->assertEquals(1, count($stack));
+    }
 
+    /**
+     * @Title("Stack pop operation test")
+     * @Step("Core operations")
+     * @Features({"Read operation"})
+     */
+    public function testPop()
+    {
+        $stack = array('foo');
         $this->assertEquals('foo', array_pop($stack));
         $this->assertEquals(0, count($stack));
     }
