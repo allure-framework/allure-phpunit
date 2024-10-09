cd0fefb8b5c2a13f5fb8062165fad82482a3c207
manishranjan-adobe <del55204@adobe.com>
2024-10-09 19:36:26 +0530

    Allure PHPUnit extension is not recognising the parameter path if the phpunit.xml.dist is placed in a different directory than root

--- src/AllureExtension.php
+++ src/AllureExtension.php
@@ -92,9 +92,16 @@ final class AllureExtension implements Extension
 
     public function bootstrap(Configuration $configuration, Facade $facade, ParameterCollection $parameters): void
     {
-        $configSource = $parameters->has('config')
-            ? $parameters->get('config')
-            : null;
+        if ($configuration->hasConfigurationFile()) {
+            $path = dirname($configuration->configurationFile());
+            $configSource = $parameters->has('config')
+                ? $path . DIRECTORY_SEPARATOR . $parameters->get('config')
+                : null;
+        } else {
+            $configSource = $parameters->has('config')
+                ? $parameters->get('config')
+                : null;
+        }
 
         $testLifecycle = $this->testLifecycle ?? $this->createTestLifecycle($configSource);
 
